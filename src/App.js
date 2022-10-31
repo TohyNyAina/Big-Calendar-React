dedf437cbdefd3fc9f3545eea08dae66e049d4f9
TohyNyAina <tohiniainarazafi@gmail.com>
2022-10-31 08:17:29 +0100

    Calendrier

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddEvent from './components/AddEvent'
+import Agenda from './components/Calendar'
+
+function App() {
+  return (
+    <div>
+      <AddEvent/> <br /> <Agenda/>
+    </div>
+  )
+}
+
+export default App
